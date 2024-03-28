f75550399bc59cd80a163b0cbf557cd0b13d07c2
iraedeus <dtotjmyanin@mail.ru>
2024-03-28 21:32:49 +0300

    refactor: Renamed generic type, removed unnecessary classes from the namespace

--- src/homework/homework_1/registry.py
+++ src/homework/homework_1/registry.py
@@ -1,19 +1,19 @@
 from typing import Callable, Generic, Optional, Type, TypeVar
 
-Parent = TypeVar("Parent")
+T = TypeVar("T")
 
 
 class AlreadyExistError(Exception):
     pass
 
 
-class Registry(Generic[Parent]):
-    def __init__(self, default: Optional[Type[Parent]]) -> None:
-        self.classes: dict[str, Type[Parent]] = dict()
-        self.default: Optional[Type[Parent]] = default
+class Registry(Generic[T]):
+    def __init__(self, default: Optional[Type[T]]) -> None:
+        self.classes: dict[str, Type[T]] = dict()
+        self.default: Optional[Type[T]] = default
 
-    def register(self, name: str) -> Callable[[Type[Parent]], Type[Parent]]:
-        def inner(cls: Type[Parent]) -> Type[Parent]:
+    def register(self, name: str) -> Callable[[Type[T]], Type[T]]:
+        def inner(cls: Type[T]) -> Type[T]:
             if name not in self.classes:
                 self.classes[name] = cls
                 return cls
@@ -22,7 +22,7 @@ class Registry(Generic[Parent]):
 
         return inner
 
-    def dispatch(self, name: str) -> Type[Parent]:
+    def dispatch(self, name: str) -> Type[T]:
         if name in self.classes:
             return self.classes[name]
         elif self.default is not None:
@@ -31,29 +31,25 @@ class Registry(Generic[Parent]):
             raise ValueError("This class is unknown")
 
 
-class ParentClass:
-    pass
-
-
-parent = Registry[ParentClass](default=None)
-
-
-@parent.register(name="AVL")
-class AVLTree(ParentClass):
-    pass
-
-
-@parent.register(name="Treap")
-class CartesianTree(ParentClass):
-    pass
-
-
-@parent.register(name="Smh")
-class SomethingElse(ParentClass):
-    pass
-
-
 if __name__ == "__main__":
+
+    class ParentClass:
+        pass
+
+    parent = Registry[ParentClass](default=None)
+
+    @parent.register(name="AVL")
+    class AVLTree(ParentClass):
+        pass
+
+    @parent.register(name="Treap")
+    class CartesianTree(ParentClass):
+        pass
+
+    @parent.register(name="Smh")
+    class SomethingElse(ParentClass):
+        pass
+
     user_choice = input("Select the class whose object you want to create: \n" "Available: AVL, Treap, Smh \n")
 
     user_class = parent.dispatch(user_choice)
