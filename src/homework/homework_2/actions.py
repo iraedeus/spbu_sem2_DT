0f07a23740c29eda2e297e5eee92075bf9edddcd
iraedeus <dtotjmyanin@mail.ru>
2024-05-26 16:05:42 +0300

    fix: some changes

--- src/homework/homework_2/actions.py
+++ src/homework/homework_2/actions.py
@@ -1,4 +1,6 @@
 import inspect
+from abc import ABCMeta, abstractmethod
+from collections import deque
 from typing import Any, Optional
 
 from src.homework.homework_1.registry import *
@@ -6,238 +8,196 @@ from src.homework.homework_1.registry import *
 ACTIONS = Registry["Action"](default=None)
 
 
-class Node:
-    def __init__(self, next: Optional["Node"], value: Any):
-        self.next: Optional[Node] = next
-        self.value = value
-
-
-class Stack:
-    def __init__(self, head: Optional[Node]) -> None:
-        self.head: Optional[Node] = head
-
-    def put(self, value: Any) -> None:
-        new_node = Node(self.head, value)
-        self.head = new_node
-
-    def pop(self) -> Any:
-        if self.head is None:
-            raise IndexError("Pop from empty Stack object")
-
-        value = self.head.value
-        new_node = self.head.next
-        self.head = new_node
-
-        return value
-
-
 class PerformedCommandStorage:
-    def __init__(self, storage: list[int], history: Stack = Stack(None)) -> None:
+    def __init__(self, storage: list[int], history: deque = deque()) -> None:
         if not isinstance(storage, list):
             raise TypeError("Storage must be list")
-        self.history: Stack = history
+        self.history: deque = history
         self.storage: list[int] = storage
 
     def apply(self, action: "Action") -> None:
-        action.do()
-        self.history.put(action)
+        action.do(self.storage)
+        self.history.append(action)
 
     def undo(self) -> None:
-        if self.history.head is None:
-            raise IndexError("Pop from empty Stack object")
-        action = self.history.head.value
-        action.undo()
-        self.history.pop()
+        action = self.history.pop()
+        action.undo(self.storage)
 
 
-class Action:
+class Action(metaclass=ABCMeta):
     def __init__(self, *args: Any) -> None:
         pass
 
-    def do(self) -> None:
+    @abstractmethod
+    def do(self, storage_list: list[int]) -> None:
         pass
 
-    def undo(self) -> None:
+    @abstractmethod
+    def undo(self, storage_list: list[int]) -> None:
         pass
 
 
 @ACTIONS.register("InsertFirst")
 class ActionInsertFirst(Action):
-    def __init__(self, storage_list: list[int], value: int):
+    def __init__(self, value: int):
         if not isinstance(value, int):
             raise TypeError("The type of the value must be an int")
         self.value = value
-        self.storage_list = storage_list
 
-    def do(self) -> None:
-        self.storage_list.insert(0, self.value)
+    def do(self, storage_list: list[int]) -> None:
+        storage_list.insert(0, self.value)
 
-    def undo(self) -> None:
-        del self.storage_list[0]
+    def undo(self, storage_list: list[int]) -> None:
+        del storage_list[0]
 
 
 @ACTIONS.register("InsertLast")
 class ActionInsertLast(Action):
-    def __init__(self, storage_list: list[int], value: int) -> None:
+    def __init__(self, value: int) -> None:
         if not isinstance(value, int):
             raise TypeError("The type of the value must be an int")
         self.value = value
-        self.storage_list = storage_list
 
-    def do(self) -> None:
-        self.storage_list.append(self.value)
+    def do(self, storage_list: list[int]) -> None:
+        storage_list.append(self.value)
 
-    def undo(self) -> None:
-        del self.storage_list[-1]
+    def undo(self, storage_list: list[int]) -> None:
+        del storage_list[-1]
 
 
 @ACTIONS.register("Swap")
 class ActionSwap(Action):
-    def __init__(self, storage_list: list[int], first_index: int, second_index: int):
+    def __init__(self, first_index: int, second_index: int):
         if not isinstance(first_index, int) or not isinstance(second_index, int):
             raise TypeError("The type of the indexes must be an int")
         self.first_index = first_index
         self.second_index = second_index
-        self.storage_list = storage_list
 
-    def do(self) -> None:
-        first_value, second_value = (
-            self.storage_list[self.first_index],
-            self.storage_list[self.second_index],
-        )
-        self.storage_list[self.first_index], self.storage_list[self.second_index] = (
-            second_value,
-            first_value,
+    def do(self, storage_list: list[int]) -> None:
+        storage_list[self.first_index], storage_list[self.second_index] = (
+            storage_list[self.second_index],
+            storage_list[self.first_index],
         )
 
-    def undo(self) -> None:
-        self.do()
+    def undo(self, storage_list: list[int]) -> None:
+        self.do(storage_list)
 
 
 @ACTIONS.register("Add")
 class ActionAdd(Action):
-    def __init__(self, storage_list: list[int], index: int, value: int):
+    def __init__(self, index: int, value: int):
         if not isinstance(value, int) or not isinstance(index, int):
             raise TypeError("The type of the value and index must be an int")
         self.index = index
         self.value = value
-        self.storage_list = storage_list
 
-    def do(self) -> None:
-        self.storage_list[self.index] += self.value
+    def do(self, storage_list: list[int]) -> None:
+        storage_list[self.index] += self.value
 
-    def undo(self) -> None:
-        self.storage_list[self.index] -= self.value
+    def undo(self, storage_list: list[int]) -> None:
+        storage_list[self.index] -= self.value
 
 
 @ACTIONS.register("Flip")
 class ActionFlip(Action):
-    def __init__(self, storage_list: list[int], index: int) -> None:
+    def __init__(self, index: int) -> None:
         self.index = index
-        self.storage_list = storage_list
 
-    def do(self) -> None:
-        self.storage_list[self.index] = -self.storage_list[self.index]
+    def do(self, storage_list: list[int]) -> None:
+        storage_list[self.index] = -storage_list[self.index]
 
-    def undo(self) -> None:
-        self.do()
+    def undo(self, storage_list: list[int]) -> None:
+        self.do(storage_list)
 
 
 @ACTIONS.register("Square")
 class ActionSquare(Action):
-    def __init__(self, storage_list: list[int], index: int) -> None:
+    def __init__(self, index: int) -> None:
         self.index = index
-        self.storage_list = storage_list
 
-    def do(self) -> None:
-        self.storage_list[self.index] = self.storage_list[self.index] ** 2
+    def do(self, storage_list: list[int]) -> None:
+        storage_list[self.index] = storage_list[self.index] ** 2
 
-    def undo(self) -> None:
-        self.storage_list[self.index] = int(self.storage_list[self.index] ** 0.5)
+    def undo(self, storage_list: list[int]) -> None:
+        storage_list[self.index] = int(storage_list[self.index] ** 0.5)
 
 
 @ACTIONS.register("Reverse")
 class ActionReverse(Action):
-    def __init__(self, storage_list: list[int]):
-        self.storage_list = storage_list
+    def __init__(self) -> None:
+        pass
 
-    def do(self) -> None:
-        self.storage_list.reverse()
+    def do(self, storage_list: list[int]) -> None:
+        storage_list.reverse()
 
-    def undo(self) -> None:
-        self.storage_list.reverse()
+    def undo(self, storage_list: list[int]) -> None:
+        storage_list.reverse()
 
 
 @ACTIONS.register("DeleteSlice")
 class ActionDeleteSlice(Action):
-    def __init__(self, storage_list: list[int], first_index: int, second_index: int) -> None:
+    def __init__(self, first_index: int, second_index: int) -> None:
         if not isinstance(first_index, int) or not isinstance(second_index, int):
             raise TypeError("The type of the indexes must be an int")
 
         self.first_index: int = first_index
         self.second_index: int = second_index
-        self.storage_list = storage_list
         self.deleted_slice: list[int] = []
 
-    def do(self) -> None:
-        if not ((0 <= self.first_index < len(self.storage_list)) and (0 <= self.second_index < len(self.storage_list))):
+    def do(self, storage_list: list[int]) -> None:
+        if not ((0 <= self.first_index < len(storage_list)) and (0 <= self.second_index < len(storage_list))):
             raise IndexError("Index out of range")
         if self.first_index > self.second_index:
             raise IndexError("First index must be less or equal then second index")
 
-        self.deleted_slice = self.storage_list[self.first_index : self.second_index + 1]
-        del self.storage_list[self.first_index : self.second_index + 1]
+        self.deleted_slice = storage_list[self.first_index : self.second_index + 1]
+        del storage_list[self.first_index : self.second_index + 1]
 
-    def undo(self) -> None:
-        self.storage_list[: self.first_index] += self.deleted_slice
+    def undo(self, storage_list: list[int]) -> None:
+        storage_list[: self.first_index] += self.deleted_slice
 
 
 @ACTIONS.register("Pop")
 class ActionPop(Action):
-    def __init__(self, storage_list: list[int], index: int) -> None:
+    def __init__(self, index: int) -> None:
         self.index = index
         self.value: int
-        self.storage_list = storage_list
 
-    def do(self) -> None:
-        self.value = self.storage_list.pop(self.index)
+    def do(self, storage_list: list[int]) -> None:
+        self.value = storage_list.pop(self.index)
 
-    def undo(self) -> None:
-        self.storage_list.insert(self.index, self.value)
+    def undo(self, storage_list: list[int]) -> None:
+        storage_list.insert(self.index, self.value)
 
 
 @ACTIONS.register("Move")
 class ActionMove(Action):
-    def __init__(self, storage_list: list[int], step: int) -> None:
+    def __init__(self, step: int) -> None:
         self.step = step
-        self.storage_list = storage_list
 
-    def do(self) -> None:
+    def do(self, storage_list: list[int]) -> None:
         if self.step == 0:
             return None
 
-        length = len(self.storage_list)
-        self.storage_list[length - self.step :] += self.storage_list[: length - self.step]
-        del self.storage_list[: length - self.step]
+        length = len(storage_list)
+        storage_list[length - self.step :] += storage_list[: length - self.step]
+        del storage_list[: length - self.step]
 
-    def undo(self) -> None:
+    def undo(self, storage_list: list[int]) -> None:
         if self.step == 0:
             return None
 
-        self.step = len(self.storage_list) - self.step
-        self.do()
+        self.step = len(storage_list) - self.step
+        self.do(storage_list)
 
 
 def parse_args(user_command: str) -> tuple[str, list[Any]]:
-    strings = user_command.split(" ")
-    return strings[0], strings[1:]
+    action, *args = user_command.split(" ")
+    return action, args
 
 
 def to_int(args: list[Any]) -> list[int]:
-    for i in range(len(args)):
-        args[i] = int(args[i])
-
-    return args
+    return list(map(int, args))
 
 
 def do_action(user_cmd_storage: PerformedCommandStorage, user_action: str, args: list[Any]) -> None:
@@ -261,7 +221,7 @@ def do_action(user_cmd_storage: PerformedCommandStorage, user_action: str, args:
         return None
 
     try:
-        user_cmd_storage.apply(action(user_cmd_storage.storage, *args))
+        user_cmd_storage.apply(action(*args))
     except IndexError as error:
         print(error)
 
@@ -289,7 +249,7 @@ def create_start_message() -> str:
 
 def main() -> None:
     print(create_start_message())
-    user_cmd_storage = PerformedCommandStorage([], Stack(None))
+    user_cmd_storage = PerformedCommandStorage([], deque())
     while True:
         user_command = input("Enter your command with args: \n")
         user_action, args = parse_args(user_command)
--- tests/homework/homework_2/test_actions.py
+++ tests/homework/homework_2/test_actions.py
@@ -9,41 +9,7 @@ from src.homework.homework_2.actions import *
 
 
 def create_test_cmd_storage(storage):
-    new_stack = Stack(None)
-    return PerformedCommandStorage(storage=storage, history=new_stack)
-
-
-def parse(fake_data):
-    lines = fake_data.split("")
-
-
-class StackTest(RuleBasedStateMachine):
-    def __init__(self):
-        super().__init__()
-
-    database = Stack(None)
-    compared_database = []
-    values = Bundle("values")
-
-    @rule(target=values, v=st.integers())
-    def add_value(self, v):
-        return v
-
-    @rule(v=values)
-    def put_to_database(self, v):
-        self.database.put(v)
-        self.compared_database.append(v)
-
-    @rule()
-    def pop_from_database(self):
-        if len(self.compared_database) > 1:
-            self.database.pop()
-            del self.compared_database[-1]
-
-    @invariant()
-    def compare(self):
-        if len(self.compared_database) != 0:
-            assert self.compared_database[-1] == self.database.head.value
+    return PerformedCommandStorage(storage=storage, history=deque())
 
 
 class TestCommandStorage:
@@ -66,7 +32,7 @@ class TestActionInsertFirst:
     )
     def test_do(self, storage, value, expected):
         cmd_storage = create_test_cmd_storage(storage)
-        cmd_storage.apply(action=ActionInsertFirst(cmd_storage.storage, value))
+        cmd_storage.apply(action=ActionInsertFirst(value))
         assert cmd_storage.storage == expected
 
     @pytest.mark.parametrize(
@@ -75,8 +41,8 @@ class TestActionInsertFirst:
     )
     def test_undo(self, storage, value_1, value_2, expected):
         cmd_storage = create_test_cmd_storage(storage)
-        cmd_storage.apply(action=ActionInsertFirst(cmd_storage.storage, value_1))
-        cmd_storage.apply(action=ActionInsertFirst(cmd_storage.storage, value_2))
+        cmd_storage.apply(action=ActionInsertFirst(value_1))
+        cmd_storage.apply(action=ActionInsertFirst(value_2))
         cmd_storage.undo()
 
         assert cmd_storage.storage == expected
@@ -85,7 +51,7 @@ class TestActionInsertFirst:
     def test_err_not_int(self, storage, value):
         cmd_storage = create_test_cmd_storage(storage)
         with pytest.raises(TypeError):
-            cmd_storage.apply(ActionInsertFirst(cmd_storage.storage, value))
+            cmd_storage.apply(ActionInsertFirst(value))
 
 
 class TestActionInsertLast:
@@ -95,7 +61,7 @@ class TestActionInsertLast:
     )
     def test_do(self, storage, value, expected):
         cmd_storage = create_test_cmd_storage(storage)
-        cmd_storage.apply(action=ActionInsertLast(cmd_storage.storage, value))
+        cmd_storage.apply(action=ActionInsertLast(value))
         assert cmd_storage.storage == expected
 
     @pytest.mark.parametrize(
@@ -104,8 +70,8 @@ class TestActionInsertLast:
     )
     def test_undo(self, storage, value_1, value_2, expected):
         cmd_storage = create_test_cmd_storage(storage)
-        cmd_storage.apply(action=ActionInsertLast(cmd_storage.storage, value_1))
-        cmd_storage.apply(action=ActionInsertLast(cmd_storage.storage, value_2))
+        cmd_storage.apply(action=ActionInsertLast(value_1))
+        cmd_storage.apply(action=ActionInsertLast(value_2))
         cmd_storage.undo()
 
         assert cmd_storage.storage == expected
@@ -114,7 +80,7 @@ class TestActionInsertLast:
     def test_err_not_int(self, storage, value):
         cmd_storage = create_test_cmd_storage(storage)
         with pytest.raises(TypeError):
-            cmd_storage.apply(ActionInsertFirst(cmd_storage.storage, value))
+            cmd_storage.apply(ActionInsertFirst(value))
 
 
 class TestActionSwap:
@@ -124,7 +90,7 @@ class TestActionSwap:
     )
     def test_do(self, storage, first_index, second_index, expected):
         cmd_storage = create_test_cmd_storage(storage)
-        cmd_storage.apply(action=ActionSwap(cmd_storage.storage, first_index, second_index))
+        cmd_storage.apply(action=ActionSwap(first_index, second_index))
         assert cmd_storage.storage == expected
 
     @pytest.mark.parametrize(
@@ -133,8 +99,8 @@ class TestActionSwap:
     )
     def test_undo(self, storage, first_index, second_index, expected):
         cmd_storage = create_test_cmd_storage(storage)
-        cmd_storage.apply(action=ActionSwap(cmd_storage.storage, first_index, second_index))
-        cmd_storage.apply(action=ActionSwap(cmd_storage.storage, first_index + 1, second_index + 1))
+        cmd_storage.apply(action=ActionSwap(first_index, second_index))
+        cmd_storage.apply(action=ActionSwap(first_index + 1, second_index + 1))
         cmd_storage.undo()
 
         assert cmd_storage.storage == expected
@@ -143,7 +109,7 @@ class TestActionSwap:
     def test_err_no_index(self, storage, first_index, second_index):
         cmd_storage = create_test_cmd_storage(storage)
         with pytest.raises(IndexError):
-            cmd_storage.apply(ActionSwap(cmd_storage.storage, first_index, second_index))
+            cmd_storage.apply(ActionSwap(first_index, second_index))
 
 
 class TestActionAdd:
@@ -153,7 +119,7 @@ class TestActionAdd:
     )
     def test_do(self, storage, index, value, expected):
         cmd_storage = create_test_cmd_storage(storage)
-        cmd_storage.apply(action=ActionAdd(cmd_storage.storage, index, value))
+        cmd_storage.apply(action=ActionAdd(index, value))
         assert cmd_storage.storage == expected
 
     @pytest.mark.parametrize(
@@ -162,8 +128,8 @@ class TestActionAdd:
     )
     def test_undo(self, storage, index, value, expected):
         cmd_storage = create_test_cmd_storage(storage)
-        cmd_storage.apply(action=ActionAdd(cmd_storage.storage, index, value))
-        cmd_storage.apply(action=ActionAdd(cmd_storage.storage, index, value))
+        cmd_storage.apply(action=ActionAdd(index, value))
+        cmd_storage.apply(action=ActionAdd(index, value))
         cmd_storage.undo()
 
         assert cmd_storage.storage == expected
@@ -172,7 +138,7 @@ class TestActionAdd:
     def test_err_no_index(self, storage, index, value):
         cmd_storage = create_test_cmd_storage(storage)
         with pytest.raises(IndexError):
-            cmd_storage.apply(ActionAdd(cmd_storage.storage, index, value))
+            cmd_storage.apply(ActionAdd(index, value))
 
 
 class TestActionFlip:
@@ -182,7 +148,7 @@ class TestActionFlip:
     )
     def test_do(self, storage, index, expected):
         cmd_storage = create_test_cmd_storage(storage)
-        cmd_storage.apply(action=ActionFlip(cmd_storage.storage, index))
+        cmd_storage.apply(action=ActionFlip(index))
         assert cmd_storage.storage == expected
 
     @pytest.mark.parametrize(
@@ -191,8 +157,8 @@ class TestActionFlip:
     )
     def test_undo(self, storage, index, expected):
         cmd_storage = create_test_cmd_storage(storage)
-        cmd_storage.apply(action=ActionFlip(cmd_storage.storage, index))
-        cmd_storage.apply(action=ActionFlip(cmd_storage.storage, index))
+        cmd_storage.apply(action=ActionFlip(index))
+        cmd_storage.apply(action=ActionFlip(index))
         cmd_storage.undo()
 
         assert cmd_storage.storage == expected
@@ -201,7 +167,7 @@ class TestActionFlip:
     def test_err_no_index(self, storage, index):
         cmd_storage = create_test_cmd_storage(storage)
         with pytest.raises(IndexError):
-            cmd_storage.apply(ActionFlip(cmd_storage.storage, index))
+            cmd_storage.apply(ActionFlip(index))
 
 
 class TestActionSquare:
@@ -211,7 +177,7 @@ class TestActionSquare:
     )
     def test_do(self, storage, index, expected):
         cmd_storage = create_test_cmd_storage(storage)
-        cmd_storage.apply(action=ActionSquare(cmd_storage.storage, index))
+        cmd_storage.apply(action=ActionSquare(index))
         assert cmd_storage.storage == expected
 
     @pytest.mark.parametrize(
@@ -220,8 +186,8 @@ class TestActionSquare:
     )
     def test_undo(self, storage, index, expected):
         cmd_storage = create_test_cmd_storage(storage)
-        cmd_storage.apply(action=ActionSquare(cmd_storage.storage, index))
-        cmd_storage.apply(action=ActionSquare(cmd_storage.storage, index))
+        cmd_storage.apply(action=ActionSquare(index))
+        cmd_storage.apply(action=ActionSquare(index))
         cmd_storage.undo()
 
         assert cmd_storage.storage == expected
@@ -230,7 +196,7 @@ class TestActionSquare:
     def test_err_no_index(self, storage, index):
         cmd_storage = create_test_cmd_storage(storage)
         with pytest.raises(IndexError):
-            cmd_storage.apply(ActionSquare(cmd_storage.storage, index))
+            cmd_storage.apply(ActionSquare(index))
 
 
 class TestActionDeleteSlice:
@@ -240,7 +206,7 @@ class TestActionDeleteSlice:
     )
     def test_do(self, storage, first_index, second_index, expected):
         cmd_storage = create_test_cmd_storage(storage)
-        cmd_storage.apply(action=ActionDeleteSlice(cmd_storage.storage, first_index, second_index))
+        cmd_storage.apply(action=ActionDeleteSlice(first_index, second_index))
         assert cmd_storage.storage == expected
 
     @pytest.mark.parametrize(
@@ -249,7 +215,7 @@ class TestActionDeleteSlice:
     )
     def test_undo(self, storage, first_index, second_index, expected):
         cmd_storage = create_test_cmd_storage(storage)
-        cmd_storage.apply(action=ActionDeleteSlice(cmd_storage.storage, first_index, second_index))
+        cmd_storage.apply(action=ActionDeleteSlice(first_index, second_index))
         cmd_storage.undo()
 
         assert cmd_storage.storage == expected
@@ -258,7 +224,7 @@ class TestActionDeleteSlice:
     def test_err_no_index(self, storage, first_index, second_index):
         cmd_storage = create_test_cmd_storage(storage)
         with pytest.raises(IndexError):
-            cmd_storage.apply(ActionDeleteSlice(cmd_storage.storage, first_index, second_index))
+            cmd_storage.apply(ActionDeleteSlice(first_index, second_index))
 
 
 class TestActionPop:
@@ -268,7 +234,7 @@ class TestActionPop:
     )
     def test_do(self, storage, index, expected):
         cmd_storage = create_test_cmd_storage(storage)
-        cmd_storage.apply(action=ActionPop(cmd_storage.storage, index))
+        cmd_storage.apply(action=ActionPop(index))
         assert cmd_storage.storage == expected
 
     @pytest.mark.parametrize(
@@ -277,7 +243,7 @@ class TestActionPop:
     )
     def test_undo(self, storage, index, expected):
         cmd_storage = create_test_cmd_storage(storage)
-        cmd_storage.apply(action=ActionPop(cmd_storage.storage, index))
+        cmd_storage.apply(action=ActionPop(index))
         cmd_storage.undo()
 
         assert cmd_storage.storage == expected
@@ -286,7 +252,7 @@ class TestActionPop:
     def test_err_no_index(self, storage, index):
         cmd_storage = create_test_cmd_storage(storage)
         with pytest.raises(IndexError):
-            cmd_storage.apply(ActionSquare(cmd_storage.storage, index))
+            cmd_storage.apply(ActionSquare(index))
 
 
 class TestActionMove:
@@ -296,7 +262,7 @@ class TestActionMove:
     )
     def test_do(self, storage, step, expected):
         cmd_storage = create_test_cmd_storage(storage)
-        cmd_storage.apply(action=ActionMove(cmd_storage.storage, step))
+        cmd_storage.apply(action=ActionMove(step))
         assert cmd_storage.storage == expected
 
     @pytest.mark.parametrize(
@@ -305,8 +271,8 @@ class TestActionMove:
     )
     def test_undo(self, storage, step, expected):
         cmd_storage = create_test_cmd_storage(storage)
-        cmd_storage.apply(action=ActionMove(cmd_storage.storage, step))
-        cmd_storage.apply(action=ActionMove(cmd_storage.storage, step))
+        cmd_storage.apply(action=ActionMove(step))
+        cmd_storage.apply(action=ActionMove(step))
         cmd_storage.undo()
 
         assert cmd_storage.storage == expected
@@ -315,7 +281,7 @@ class TestActionMove:
     def test_err_no_index(self, storage, step):
         cmd_storage = create_test_cmd_storage(storage)
         with pytest.raises(IndexError):
-            cmd_storage.apply(ActionSquare(cmd_storage.storage, step))
+            cmd_storage.apply(ActionSquare(step))
 
 
 def get_output(string: str):
@@ -372,6 +338,3 @@ def test_main_scenario_err(actions, expected_err, monkeypatch, capsys):
     captured = capsys.readouterr().out
 
     assert captured.split("\n")[-2] == expected_err
-
-
-StackTestCase = StackTest.TestCase
