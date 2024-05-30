81e4fd8211c71448b3eea12d4762a134974d87b0
iraedeus <dtotjmyanin@mail.ru>
2024-05-30 22:18:21 +0300

    fix: Endless work has been fixed, in the case when the user entered a sheet with the same names and the --unique flag

--- src/exam/exam_3/main.py
+++ src/exam/exam_3/main.py
@@ -23,17 +23,20 @@ def get_path_between_two(
     return multiprocess_find_path(result_path, start_page, end_page, processor_cnt, unique)
 
 
-def get_all_path(pages: list[str], processor_cnt: int, unique: bool) -> None:
-    output: list[str] = []
-    for i in range(len(pages) - 1):
-        get_path_between_two(output, pages[i], pages[i + 1], processor_cnt, unique)
+def get_all_path(pages: list[str], processor_cnt: int, unique: bool = False) -> None:
+    if unique and len(pages) > len(set(pages)):
+        print("There should not be identical pages in your link list")
+    else:
+        output: list[str] = []
+        for i in range(len(pages) - 1):
+            get_path_between_two(output, pages[i], pages[i + 1], processor_cnt, unique)
 
-    output.append(f"https://en.wikipedia.org/wiki/{pages[-1]}")
-    number = 1
-    print(output)
-    print("\nThe final path found is:")
-    for link in output:
-        print(f"{number}) {link}")
+        output.append(f"https://en.wikipedia.org/wiki/{pages[-1]}")
+        number = 1
+        print(output)
+        print("\nThe final path found is:")
+        for link in output:
+            print(f"{number}) {link}")
         number += 1
 
 
